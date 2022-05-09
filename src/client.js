04189c45a7c3398de0c1e1a9d965a86a59dbb460
Yetife <tifetreats21@gmail.com>
2022-05-09 23:36:02 +0100

    New files

--- src/client.js
+++ src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = sanityClient({
-    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+    projectId: "i745u79y",
     dataset: 'production',
     apiVersion: '2022-05-07',
     useCdn: true,
-    token: process.env.REACT_APP_SANITY_TOKEN,
+    token: "sk1bGbcbIDHtTVxY2fAxkx1FPkUIzpwbczNvFGAGDL4S2loYVSOpkjirXrn45V98sofjx2ANXCzMqq5Glwz8eQ253q5eJGJHLUX9HgJaBEEBjqkNzMejJXKvrMo7yz1arXrmmSq5Qu63YBVMSNYmL0boGIs2GWpoztIukuYSc2EWfghbVhzQ",
 });
 
 const builder = imageUrlBuilder(client)
--- src/container/about/About.jsx
+++ src/container/about/About.jsx
@@ -1,9 +1,10 @@
-import React, {useEffect, useState} from 'react';
-import './about.scss'
-import images from "../../constants/images";
-import { motion } from 'framer-motion/dist/framer-motion'
-import {client, urlFor} from "../../client";
-import {AppWrap} from "../../wrapper";
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+
+import { AppWrap} from '../../wrapper';
+import './about.scss';
+import { urlFor, client } from '../../client';
+import MotionWrap from "../../wrapper/MotionWrap";
 
 const About = () => {
     const [abouts, setAbouts] = useState([]);
@@ -11,30 +12,27 @@ const About = () => {
     useEffect(() => {
         const query = '*[_type == "abouts"]';
 
-        client.fetch(query)
-            .then((data) => setAbouts(data))
-    }, [])
+        client.fetch(query).then((data) => {
+            setAbouts(data);
+        });
+    }, []);
 
     return (
         <>
-            <h2 className={"head-text"}>
-                I Know that <span>Good Development</span>
-                <br/>
-                means <span>Good Business</span>
-            </h2>
+            <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
 
             <div className="app__profiles">
                 {abouts.map((about, index) => (
                     <motion.div
-                        whileInView={{opacity: 1}}
-                        whileHover={{scale: 1.1}}
-                        transition={{duration: 0.5, type: 'tween'}}
+                        whileInView={{ opacity: 1 }}
+                        whileHover={{ scale: 1.1 }}
+                        transition={{ duration: 0.5, type: 'tween' }}
                         className="app__profile-item"
                         key={about.title + index}
                     >
-                        <img src={urlFor(about.imgUrl)} alt={about.title}/>
-                        <h2 className={"bold-text"} style={{marginTop: 20}}>{about.title}</h2>
-                        <p className={"p-text"} style={{marginTop: 10}}>{about.description}</p>
+                        <img src={urlFor(about.imgUrl)} alt={about.title} />
+                        <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+                        <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
                     </motion.div>
                 ))}
             </div>
@@ -42,4 +40,8 @@ const About = () => {
     );
 };
 
-export default AppWrap(About, 'about');
+export default AppWrap(
+    MotionWrap(About, 'app__about'),
+    'about',
+    'app__whitebg',
+);
